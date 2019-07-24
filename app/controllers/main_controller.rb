22c039ddb2fbebd707f211ef15825c7d7b3773fb
stai1 <stai1@scu.edu>
2019-07-23 20:29:18 -0700

    Absence of 'state', 'code' query params trigger landing page render

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,6 @@
 class MainController < ApplicationController
   def index
-    if request.query_parameters.empty?
+    if !(request.query_parameters.include?("state") && request.query_parameters.include?("code"))
       @url = "https://www.strava.com/oauth/authorize?client_id=36433&response_type=code&redirect_uri="+Rails.configuration.redirect_uri+"&approval_prompt=force"
       render "landing"
     end
