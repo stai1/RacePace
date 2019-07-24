a53df34e0bbec98c99b6a198152393e5441a3002
stai1 <stai1@scu.edu>
2019-07-23 21:19:20 -0700

    Complete oauth2 handshake

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,14 @@
+require 'net/http'
+
 class MainController < ApplicationController
   def index
     if !(request.query_parameters.include?("state") && request.query_parameters.include?("code"))
       @url = "https://www.strava.com/oauth/authorize?client_id=36433&response_type=code&redirect_uri="+Rails.configuration.redirect_uri+"&approval_prompt=force"
       render "landing"
+    else
+      postData = Net::HTTP.post_form(URI.parse("https://www.strava.com/oauth/token"), {"grant_type"=>"authorization_code", "code"=>request.query_parameters["code"],"redirect_uri"=>Rails.configuration.redirect_uri,"client_id"=>Rails.configuration.client_id,"client_secret"=>Rails.application.credentials.client_secret})
+      parsed = JSON.parse(postData.body)
+      @access_token = parsed["access_token"]
     end
   end
 end
